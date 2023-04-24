3087b1d97c2a746a321c1854134e66532c3f2ad4
GRHiggins <gabrielrhiggins@gmail.com>
2023-04-24 15:09:36 +0100

    Update tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,10 @@
-import pandas as pd
+from pathlib import Path
 
-from pipelines import Pipeline, Check, Transform
-from pipelines.utils import contains_no_duplicates, contains_no_nulls, contains_columns, is_less_than
+import pandas as pd
+import pytest
+
+from pipelines import Pipeline, Check, Transform, CheckFailed
+from pipelines.utils import contains_columns, is_less_than
 
 
 def add_one(data: pd.DataFrame):
@@ -24,22 +27,31 @@ def add_one_hundred(data: pd.DataFrame):
     return data + 100
 
 
+class TestPipelines:
+
+    data = pd.read_json(Path(__file__).parent.joinpath('data.json'))
+
+    def test_contains_columns(self):
+        pl = Pipeline()
+        pl.add_input(self.data.copy())
+        pl.add_transform(Transform(add_one))
+        pl.add_check(Check(contains_columns, ['add_one'], [['a', 'b']]))  # True
+        pl.run()
+
+    def test_less_than(self):
+        pl = Pipeline()
+        pl.add_input(self.data.copy())
+        pl.add_transform(Transform(add_one))
+        pl.add_check(Check(is_less_than, ['add_one'], [100]))  # True
+        pl.run()
+
+    def test_not_less_than(self):
+        pl = Pipeline()
+        pl.add_input(self.data.copy())
+        pl.add_transform(Transform(add_one_hundred))
+        pl.add_check(Check(is_less_than, ['add_one_hundred'], [100]))  # False
+        with pytest.raises(CheckFailed):
+            pl.run()
+
 if __name__ == '__main__':
-    data = pd.DataFrame([{'a': 1, 'b': 2}, {'a': 3, 'b': 4}])
-
-    pl = Pipeline()
-    pl.add_input(data)
-    pl.add_transform(Transform(add_one))
-    pl.add_transform(Transform(add_one_hundred))
-
-    pl.add_check(Check(contains_no_duplicates, ['add_one']))  # True
-    pl.add_check(Check(contains_no_nulls, ['add_one', 'add_one_hundred']))  # True
-    pl.add_check(Check(contains_columns, ['add_one'], [['a', 'b']]))  # True
-    pl.add_check(Check(is_less_than, ['add_one'], [100]))  # True
-    # pl.add_check(Check(is_less_than, ['add_one_hundred'], [100]))  # False
-    # pl.add_check(Check(contains_columns, ['add_one'], [['a', 'd']]))  # False
-
-    print(pl)
-    print(pl.steps)
-
-    pl.run()
+    pytest.main()
